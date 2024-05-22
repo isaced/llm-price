706ce36a6e81aa1d376d69f903ab6e588e5c7abe
isaced <isaced@163.com>
2024-05-22 08:41:38 +0800

    Add footer & update prices

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { ModelInfo, columns } from "./table/columns"
 import { DataTable } from "./table/data-table"
-const fs = require('fs');
+import fs from 'fs'
+import Image from "next/image";
 
 /**
  * load data from ../data/*.json
@@ -10,15 +11,22 @@ async function getData(): Promise<ModelInfo[]> {
 
   const files = fs.readdirSync('data')
   for (const file of files) {
-    const data = fs.readFileSync(`data/${file}`)
+    const data = fs.readFileSync(`data/${file}`).toString()
     const providerData = JSON.parse(data) as {
-      provider: string, prices: {
+      provider: string,
+      currency: 'USD' | 'CNY',
+      prices: {
         model: string
         oneMInputTokenPrice: number
         oneMOutputPrice: number
       }[]
     }
-    list.push(...providerData.prices.map(item => ({ provider: providerData.provider, ...item })))
+    list.push(...providerData.prices.map(item => ({
+      provider: providerData.provider,
+      ...item,
+      oneMInputTokenPrice: providerData.currency === 'USD' ? item.oneMInputTokenPrice * 7 : item.oneMInputTokenPrice,
+      oneMOutputPrice: providerData.currency === 'USD' ? item.oneMOutputPrice * 7 : item.oneMOutputPrice,
+    })))
   }
   console.log(list)
   return list
@@ -35,10 +43,25 @@ export default async function Home() {
           LLM Price
         </div>
         <div>
-          LLM Price is a simple app that displays the price of LLM.
+          Open source project to collect and display the price of LLM model.
         </div>
       </div>
       <DataTable columns={columns} data={data} />
+      <footer>
+        <div className="flex">
+          <div>
+            <a href="https://github.com/isaced/llm-price" target="_blank" className="inline text-blue-500">
+              <Image
+                src="/github.svg"
+                alt="GitHub Logo"
+                className="dark:invert"
+                width={24}
+                height={24}
+              />
+            </a>
+          </div>
+        </div>
+      </footer>
     </main>
   );
 }
